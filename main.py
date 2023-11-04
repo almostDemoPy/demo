4971b742b177ff6e49cda53374010f15540c2c2a
demo <dazanderofficials@gmail.com>
2023-11-04 10:41:32 +0800

    Initialized bot

---
+++ main.py
@@ -0,0 +1,42 @@
+import asyncio
+import discord
+import os
+import traceback
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+  command_prefix = "demo.",
+  help_command = None,
+  intents = intents,
+  activity = discord.Activity(
+    type = discord.ActivityType.custom,
+    name = " ",
+    state = "Hello !"
+  )
+)
+
+async def load():
+  for folder in os.listdir('cogs'):
+    for file in os.listdir(f'cogs/{folder}'):
+      if file.endswith('.py'):
+        await bot.load_extension(
+          f'cogs.{folder}.{file[:-3]}'
+        )
+
+async def main():
+  await load()
+  async with bot:
+    await bot.start(
+      os.getenv("token")
+    )
+
+try:
+  asyncio.run(main())
+except:
+  traceback.print_exc()
