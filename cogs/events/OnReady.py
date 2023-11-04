f8e275c3cb582dc7350cd3b996a2843721f6e72e
demo <dazanderofficials@gmail.com>
2023-11-04 10:43:22 +0800

    added on_ready event listener

---
+++ cogs/events/OnReady.py
@@ -0,0 +1,19 @@
+import discord
+import traceback
+from discord.ext import commands
+
+class OnReady(commands.Cog):
+  def __init___(self, bot):
+    self.bot = bot
+    print(
+      "Loaded event : on_ready()"
+    )
+
+  @commands.Cog.listener()
+  async def on_ready(self):
+    print(
+      "< DEMO > is online"
+    )
+
+async def setup(bot):
+  await bot.add_cog(OnReady(bot))
