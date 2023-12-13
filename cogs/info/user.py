80d8c4b538eb6757f431b498b423fae932637444
demo <dazanderofficials@gmail.com>
2023-12-13 21:46:30 +0800

    modified : implemented different response for self

--- cogs/info/user.py
+++ cogs/info/user.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import traceback
+from datetime import datetime
 from discord import app_commands, ui
 from discord.ext import commands
 from discord.utils import get
@@ -291,6 +292,7 @@ class UserInfoSelectMenu(ui.Select):
 class UserCommands(commands.GroupCog, name = "user", description = "user commands"):
   def __init__(self, bot):
     self.bot = bot
+    self.startup = datetime.now()
     self.bot.tree.add_command(
       app_commands.ContextMenu(
         name = "Info",
@@ -307,6 +309,54 @@ class UserCommands(commands.GroupCog, name = "user", description = "user command
     try:
       response = interaction.response
       user = interaction.user
+      if member == self.bot.user:
+        guild = interaction.guild
+        botMember = guild.me
+        owner = guild.get_member(self.bot.owner_id)
+        if owner is not None:
+          botOwner = owner.mention
+        else:
+          botOwner = "` demoutrei `"
+        embed = discord.Embed(
+          title = self.bot.user.display_name,
+          description = f"""
+          **Developer** : {botOwner}
+          **Developer ID** : ||` 1057104290600189972 `||
+          **User ID** : ||` {self.bot.user.id} `||
+          **Created** : <t:{int(self.bot.user.created_at.timestamp())}:R>
+          **Joined** : <t:{int(botMember.joined_at.timestamp())}:R>
+          """,
+          color = 0x2b2d31
+        ).set_thumbnail(
+          url = self.bot.user.display_avatar
+        ).add_field(
+          name = "Command Prefix :",
+          value = "> ` demo.<command_name> `",
+          inline = True
+        ).add_field(
+          name = "Guild Count :",
+          value = f"> ` {len(self.bot.guilds):,} `",
+          inline = True
+        ).add_field(
+          name = "User Count :",
+          value = f"> ` {len(self.bot.users):,} `",
+          inline = True
+        ).add_field(
+          name = "Online Since :",
+          value = f"> <t:{int(self.startup.timestamp())}:R>",
+          inline = True
+        )
+        view = ui.View().add_item(
+          ui.Button(
+            label = "Support Server",
+            url = "https://discord.gg/mXSXzc4SJB"
+          )
+        )
+        await response.send_message(
+          embed = embed,
+          view = view
+        )
+        return
       view = ui.View().add_item(
         UserInfoSelectMenu(self.bot, user, member)
       )
