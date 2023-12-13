c185479f722943f3e6e306b7bdd708b482a4cfed
demo <dazanderofficials@gmail.com>
2023-12-13 21:25:48 +0800

    new slash command : /thesaurus antonym

--- cogs/utility/thesaurus.py
+++ cogs/utility/thesaurus.py
@@ -9,6 +9,7 @@ class Thesaurus(commands.GroupCog, name = "thesaurus", description = "Thesaurus 
   def __init__(self, bot):
     self.bot = bot
     print("Loaded command : /thesaurus synonym")
+    print("Loaded command : /thesaurus antonym")
 
   @app_commands.command(
     name = "synonym",
@@ -54,6 +55,56 @@ class Thesaurus(commands.GroupCog, name = "thesaurus", description = "Thesaurus 
     )
 
   @thesaurusSynonym.error
+  async def error(self, interaction : discord.Interaction, error):
+    traceback.print_exc()
+
+  @app_commands.command(
+    name = "antonym",
+    description = "Retrieve a antonym of a word"
+  )
+  @app_commands.describe(
+    word = "Word to retrieve the antonym of"
+  )
+  async def thesaurusAntonym(self, interaction : discord.Interaction, word : str):
+    response = interaction.response
+    user = interaction.user
+    await response.defer(
+      ephemeral = True,
+      thinking = True
+    )
+    antonymsList = []
+    for antonym in wordnet.synsets(word):
+      for l in antonym.lemmas():
+        if l.antonyms():
+          antonymsList.append(
+            l.antonyms()[0].name()
+          )
+    if not antonymsList:
+      err = discord.Embed(
+        description = f"The word ` {word} ` does not have antonyms or is not a valid word",
+        color = 0xff3131
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      await interaction.followup.send(
+        embed = err
+      )
+      return
+    wordSynonyms = ", ".join(list(set(antonymsList)))
+    embed = discord.Embed(
+      title = word.capitalize(),
+      description = f"""
+      **Antonyms** :
+      > {wordSynonyms}
+      """,
+      color = 0x2b2d31
+    )
+    await interaction.followup.send(
+      embed = embed
+    )
+  
+  @thesaurusAntonym.error
   async def error(self, interaction : discord.Interaction, error):
     traceback.print_exc()
 
