b28c42b211ab80dba0aa44872ebaf94b0d59c85e
demo <dazanderofficials@gmail.com>
2023-12-24 09:43:59 +0800

    added slash commands : /config channel farewell | /config channel logs

--- cogs/moderation/config.py
+++ cogs/moderation/config.py
@@ -1,6 +1,6 @@
 import discord
 import traceback
-from db import *
+from assets.db import *
 from discord import app_commands, ui
 from discord.ext import commands
 
@@ -81,7 +81,159 @@ class GuildConfig(commands.GroupCog, name = "config", description = "guild confi
         ephemeral = True
       )
 
+  @channel.command(
+    name = "farewell",
+    description = "Configure this guild's farewell channel"
+  )
+  @app_commands.describe(
+    channel = "Channel to set it to"
+  )
+  @app_commands.default_permissions(
+    administrator = True
+  )
+  async def configChannelFarewell(self, interaction : discord.Interaction, channel : discord.TextChannel = None):
+    response = interaction.response
+    user = interaction.user
+    guild = interaction.guild
+    if user != guild.owner:
+      err = discord.Embed(
+        description = "Only the Guild Owner can execute this command",
+        color = 0xff3131
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      await response.send_message(
+        embed = err,
+        ephemeral = True
+      )
+      return
+    config = Config(guild)
+    if channel is None:
+      if config.farewell.channel_id is None:
+        err = discord.Embed(
+          description = "` Farewell Channel ` is already set to ` None `",
+          color = 0xff3131
+        ).set_author(
+          name = self.bot.user.display_name,
+          icon_url = self.bot.user.display_avatar
+        )
+        await response.send_message(
+          embed = err,
+          ephemeral = True
+        )
+        return
+      embed = discord.Embed(
+        description = f"Successfully set ` Farewell Channel ` to : ` None `",
+        color = 0x39ff14
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      config.farewell.clear()
+      await response.send_message(
+        embed = embed,
+        ephemeral = True
+      )
+    else:
+      embed = discord.Embed(
+        description = f"Successfully set ` Farewell Channel ` to : {channel.mention}",
+        color = 0x39ff14
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      config.farewell.update(channel)
+      await response.send_message(
+        embed = embed,
+        ephemeral = True
+      )
+
+  @channel.command(
+    name = "logs",
+    description = "Configure this guild's logs channel"
+  )
+  @app_commands.describe(
+    channel = "Channel to set it to",
+    action = "Logs action to configure"
+  )
+  @app_commands.default_permissions(
+    administrator = True
+  )
+  @app_commands.choices(
+    action = [
+      app_commands.Choice(
+        name = "OnMessageEdit",
+        value = "OnMessageEdit"
+      )
+    ]
+  )
+  async def configChannelLogs(
+    self,
+    interaction : discord.Interaction,
+    action : str,
+    channel : discord.TextChannel = None
+  ):
+    response = interaction.response
+    user = interaction.user
+    guild = interaction.guild
+    if user != guild.owner:
+      err = discord.Embed(
+        description = "Only the Guild Owner can execute this command",
+        color = 0xff3131
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      await response.send_message(
+        embed = err,
+        ephemeral = True
+      )
+      return
+    config = Config(guild)
+    if channel is None:
+      if config.logs.get(action).channel_id is None:
+        err = discord.Embed(
+          description = f"` Logs Channel ` of action ` {action} ` is already set to ` None `",
+          color = 0xff3131
+        ).set_author(
+          name = self.bot.user.display_name,
+          icon_url = self.bot.user.display_avatar
+        )
+        await response.send_message(
+          embed = err,
+          ephemeral = True
+        )
+        return
+      embed = discord.Embed(
+        description = f"Successfully set ` Logs Channel ` of action ` {action} ` to : ` None `",
+        color = 0x39ff14
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      config.logs.clear(action)
+      await response.send_message(
+        embed = embed,
+        ephemeral = True
+      )
+    else:
+      embed = discord.Embed(
+        description = f"Successfully set ` Logs Channel ` of action ` {action} ` to : {channel.mention}",
+        color = 0x39ff14
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      config.logs.update(action, channel)
+      await response.send_message(
+        embed = embed,
+        ephemeral = True
+      )
+
   @configChannelWelcome.error
+  @configChannelFarewell.error
+  @configChannelLogs.error
   async def error(self, interaction : discord.Interaction, error):
     traceback.print_exc()
 
