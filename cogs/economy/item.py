7dd2b3d8e5ec81b9a69621178b1a0266f45b11f7
demo <dazanderofficials@gmail.com>
2024-01-01 12:26:13 +0800

    added slash command : /item info

--- cogs/economy/item.py
+++ cogs/economy/item.py
@@ -45,7 +45,65 @@ class ItemCommands(commands.GroupCog, name = "item", description = "item command
     item = Item(item)
     await item.execute(interaction, member, amount)
 
+  @app_commands.command(
+    name = "info",
+    description = "Retrieve the info of an item"
+  )
+  @app_commands.describe(
+    name = "Name of an item"
+  )
+  async def itemInfo(
+    self,
+    interaction : discord.Interaction,
+    name : str
+  ):
+    response = interaction.response
+    user = interaction.user
+    if get_item(name) is None:
+      err = discord.Embed(
+        description = f"There is no such item named ` {name} `",
+        color = 0xff3131
+      ).set_author(
+        name = self.bot.user.display_name,
+        icon_url = self.bot.user.display_avatar
+      )
+      await response.send_message(
+        embeed = err,
+        ephemeral = True
+      )
+      return
+    item = Item(name)
+    embed = discord.Embed(
+      title = item.name,
+      description = f"""
+> {item.description}
+      """,
+      color = 0x2b2d31
+    ).set_author(
+      name = user.display_name,
+      icon_url = user.display_avatar
+    )
+    if item.cost is not None:
+      embed.add_field(
+        name = "Cost :",
+        value = f"> ` {item.cost[list(item.cost.keys())[0]]} ` {list(item.cost.keys())[0]}",
+        inline = True
+      )
+    embed.add_field(
+      name = "Useable :",
+      value = f"> ` {str(item.useable).capitalize()} `",
+      inline = True
+    ).add_field(
+      name = "Sellable :",
+      value = f"> ` {str(item.sellable).capitalize()} `",
+      inline = True
+    )
+    await response.send_message(
+      embed = embed
+    )
+
   @itemUse.error
+  @itemInfo.error
   async def error(self, interaction : discord.Interaction, error):
     traceback.print_exc()
 
