7452f2eed1763ddf2c2caf98e17bb835aab86112
demo <dazanderofficials@gmail.com>
2024-01-01 14:02:40 +0800

    modified

--- cogs/economy/levelling.py
+++ cogs/economy/levelling.py
@@ -13,9 +13,11 @@ class LevellingCog(commands.Cog):
   async def on_message(self, message):
     try:
       author = message.author
+      if author.bot:
+        return
       economy = Economy(author)
-      economy.add(5)
-      if economy.levelling.level == int(economy.levelling.experience ** ( 1 / 8)):
+      economy.levelling.add(5)
+      if economy.levelling.level + 1 == int(economy.levelling.experience ** ( 1 / 8)):
         economy.levelling.level_up()
         embed = discord.Embed(
           description = f"Congratulations {author.mention} ! You levelled up to ` Level {economy.levelling.level:,} ` !",
@@ -31,6 +33,51 @@ class LevellingCog(commands.Cog):
         )
     except:
       traceback.print_exc()
+
+  @app_commands.command(
+    name = "level",
+    description = "View your current level"
+  )
+  async def level(self, interaction : discord.Interaction):
+    response = interaction.response
+    user = interaction.user
+    economy = Economy(user)
+    embed = discord.Embed(
+      color = 0x2b2d31
+    ).set_thumbnail(
+      url = user.display_avatar
+    )
+    if economy.levelling.experience == 0:
+      embed.add_field(
+        name = "Current Ranking :",
+        value = "> ` Unranked `",
+        inline = True
+      )
+    else:
+      lb = get_leaderboard("levelling")
+      ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+      embed.add_field(
+        name = "Current Ranking :",
+        value = f"> ` {ordinal(lb.index(str(user.id)) + 1)} Place `",
+        inline = True
+      )
+    embed.add_field(
+      name = "Level :",
+      value = f"> ` {economy.levelling.level:,} `",
+      inline = True
+    ).add_field(
+      name = "Experience :",
+      value = f"> ` {economy.levelling.experience:,} `",
+      inline = True
+    )
+    await response.send_message(
+      embed = embed,
+      ephemeral = True
+    )
+
+  @level.error
+  async def error(self, interaction : discord.Interaction, error):
+    traceback.print_exc()
 
 async def setup(bot):
   await bot.add_cog(LevellingCog(bot))
